f10522d21ade08e3cb78e2a326b1511301d95ab8
ReabetsweJankie <jankiereabetswe@gmail.com>
2026-03-04 14:13:23 +0000

    Created HashExperiment structure with contraints

--- HashExperiment.java
+++ HashExperiment.java
@@ -1,1 +1,13 @@
 //comment 1
+import java.util.*;
+
+public class HashExperiment{
+
+    static final int N=1<<20;
+    static final int DATA SIZE = 950 000;
+    static final int M 1_000_003;
+    static final int RUNS =30;
+    
+    static KeyValue[] data =new KeyValue[DATA_SIZE];
+    //comment 2
+}
